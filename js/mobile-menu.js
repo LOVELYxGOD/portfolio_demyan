f113cdbaf5cc08d231450716e0740f0fe7cb2f9c
LOVELY <kotagindeman@gmail.com>
2025-11-21 17:56:28 +0300

    refactor: update code

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,16 @@
-const burger = document.querySelector('#burger');
+const header = document.querySelector('#header');
+const burger = header.querySelector('#burger');
+const menu = header.querySelector('#menu');
 
-burger.addEventListener('click', function() {
-    burger.classList.toggle('active');
-})
+function setStyleMobileMenu(event) {
+    const isBurger = event.target.closest('#burger') ;
+    isBurger.classList.toggle('active')
+    menu.classList.toggle('active')
+    // домашку выполнить ЗДЕСЬ
+}
+
+header.addEventListener('click',(event)=> setStyleMobileMenu(event)) 
+
+// 1. Найти и прочитать инфорамацию о дата-атрибутах(js-учебник)
+// 2. В html всем якорным сылкам дать дата-атрибут(без значения)
+// 3. При клике проверять что элемент содержит дата атрибут(сылка)-закрыть меню
