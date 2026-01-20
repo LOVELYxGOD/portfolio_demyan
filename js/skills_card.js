7632648d7b7f7146d7714a099e0e30f333ed227d
LOVELY <kotagindeman@gmail.com>
2026-01-20 18:52:25 +0300

    feat: add base style for animation

--- js/skills_card.js
+++ js/skills_card.js
@@ -1,10 +1,20 @@
 const skillsContainer = document.querySelector('#list-skill-card');
+const fade = skillsContainer.querySelector('#fade')
 
 function toggleSkillCard(event) {
     const isSkillCard = event.target.closest('[data-skill-card]');
-    
+    const isClose =  event.target.closest('[data-close]');
+
     if (isSkillCard) {
-        isSkillCard.classList.toggle('active');
+        fade.classList.add('open')
+        isSkillCard.classList.add('active');
+        console.log('карточка');
+        
+    }
+    if (isClose) {
+        isClose.closest('[data-skill-card]').classList.remove('active')
+        console.log(isClose);
+        fade.classList.remove('open')
     }
 }
 
