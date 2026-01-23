96d539f93d5dab19941ae35876bb120a7f9dc919
LOVELY <kotagindeman@gmail.com>
2026-01-23 17:07:36 +0300

    feat: add lock for card

--- js/skills_card.js
+++ js/skills_card.js
@@ -1,5 +1,6 @@
 const skillsContainer = document.querySelector('#list-skill-card');
 const fade = skillsContainer.querySelector('#fade')
+const body = document.body;
 
 function toggleSkillCard(event) {
     const isSkillCard = event.target.closest('[data-skill-card]');
@@ -8,11 +9,14 @@ function toggleSkillCard(event) {
     if (isSkillCard) {
         fade.classList.add('open')
         isSkillCard.classList.add('active');
+        body.classList.add('no-scroll');
         console.log('карточка');
         
     }
     if (isClose) {
         isClose.closest('[data-skill-card]').classList.remove('active')
+        isClose.classList.remove('no-scroll')
+        body.classList.remove('no-scroll');
         console.log(isClose);
         fade.classList.remove('open')
     }
