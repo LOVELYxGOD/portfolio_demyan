334d437c2ec24b1a72ecd445aa995d4841c5a0d2
LOVELY <kotagindeman@gmail.com>
2026-02-17 18:49:04 +0300

    feat: add wraper hidden and lenght

--- js/slider.js
+++ js/slider.js
@@ -8,8 +8,17 @@ class Slider {
             this.sliderElement = document.querySelector(selector)
         }
     }
+
+    builder(){
+        const wraperHiden = document.createElement('div')
+        const lenght = document.createElement('div')
+        this.sliderElement.append(wraperHiden)
+        wraperHiden.append(lenght)
+    }
 }
 
 const mySlider = new Slider()
 mySlider.getElement('#slider')
 console.log(mySlider);
+mySlider.builder()
+
